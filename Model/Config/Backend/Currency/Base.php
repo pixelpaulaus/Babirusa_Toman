8edcd3428027fd0fe6324186433a8675e8dd130e
Paul <paul@pixelpaul.com>
2018-03-26 19:32:03 +1100

    Update Base.php

--- Model/Config/Backend/Currency/Base.php
+++ Model/Config/Backend/Currency/Base.php
@@ -1,12 +1,12 @@
 <?php
-namespace Babirusa\Toman\Model\Config\Backend\Currency;
+namespace Magcurrency\Bitcoin\Model\Config\Backend\Currency;
 
 class Base extends \Magento\Config\Model\Config\Backend\Currency\Base
 {
     protected function _getInstalledCurrencies()
     {
         $_installed = parent::_getInstalledCurrencies();
-        array_unshift($_installed , 'IRT');
+        array_unshift($_installed , 'BTC');
         return $_installed;
     }
 }
