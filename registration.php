d3152574e2aca8847a7e586f4e8b45e2f707c091
Paul <paul@pixelpaul.com>
2018-03-26 19:30:48 +1100

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,3 +1,3 @@
 <?php
 use \Magento\Framework\Component\ComponentRegistrar;
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magcurrency_bitcoin', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magcurrency_Bitcoin', __DIR__);
