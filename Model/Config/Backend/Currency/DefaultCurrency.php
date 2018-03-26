ebf44575d46366679cc5221301c2ec03f5797670
Paul <paul@pixelpaul.com>
2018-03-26 19:32:24 +1100

    Update DefaultCurrency.php

--- Model/Config/Backend/Currency/DefaultCurrency.php
+++ Model/Config/Backend/Currency/DefaultCurrency.php
@@ -1,12 +1,12 @@
 <?php
-namespace Babirusa\Toman\Model\Config\Backend\Currency;
+namespace Magcurrency\Bitcoin\Model\Config\Backend\Currency;
 
 class DefaultCurrency extends \Magento\Config\Model\Config\Backend\Currency\DefaultCurrency
 {
     protected function _getInstalledCurrencies()
     {
         $_installed = parent::_getInstalledCurrencies();
-        array_unshift($_installed , 'IRT');
+        array_unshift($_installed , 'BTC');
         return $_installed;
     }
 }
